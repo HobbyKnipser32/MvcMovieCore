8b8ae52ba8b3a4c38b650b9bb498c0415ff1a818
Stefan Lüttich <SLuettich@cellent.de>
2021-12-02 16:53:31 +0100

    In der View Practice hinzugefügt.

--- MvcMoviesCore/Models/MoviesPerson.cs
+++ MvcMoviesCore/Models/MoviesPerson.cs
@@ -7,7 +7,9 @@ namespace MvcMoviesCore.Models
         public System.Guid MoviesId { get; set; }
 
         public System.Guid PersonId { get; set; }
-    
+
+        public string Practices { get; set; }
+
         public virtual Movies Movies { get; set; }
 
         public virtual Person Person { get; set; }
