3abf33564233f46d848490ed71cc9691db4da9d4
Stefan Luettich <ST20050591@wipro.com>
2024-04-17 15:13:17 +0200

    Try to use ajax

---
+++ MvcMoviesCore/ApiController/ApiMoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMoviesCore.ApiController
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ApiMoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
