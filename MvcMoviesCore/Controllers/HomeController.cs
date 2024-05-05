6b2360e3fd95aa6a322bd05a979d194603aea4e8
Stefan Lüttich <mymail0022-microsoft@yahoo.com>
2024-05-05 19:33:25 +0200

    Add adultonly

--- MvcMoviesCore/Controllers/HomeController.cs
+++ MvcMoviesCore/Controllers/HomeController.cs
@@ -1,31 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MvcMoviesCore.Models;
 using System.Diagnostics;
 using System.Linq;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace MvcMoviesCore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly MvcMovieCoreContext _context;
+        private readonly IConfiguration configuration;
+        private readonly bool showAdult;
 
-        public HomeController(MvcMovieCoreContext context)
+        public HomeController(MvcMovieCoreContext context, IConfiguration configuration)
         {
             _context = context;
+            this.configuration = configuration;
+            showAdult = this.configuration.GetValue<bool>("AppSettings:ShowAdult");
         }
 
         public IActionResult Index()
         {
-            var movies = _context.Movies
-                .Include(m => m.Genre)
-                .Include(m => m.RecordCarrier)
-                .Include(m => m.StorageLocation)
-                .Where(w => w.LastView == null)
-                .OrderByDescending(o => o.CreateDate)
-                .ThenBy(t => t.Name)
-                .Take(20)
-                .AsQueryable();
+            IQueryable<Movies> movies;
+
+            if (showAdult)
+                movies = _context.Movies
+                    .Include(m => m.Genre)
+                    .Include(m => m.RecordCarrier)
+                    .Include(m => m.StorageLocation)
+                    .Where(w => w.LastView == null)
+                    .OrderByDescending(o => o.CreateDate)
+                    .ThenBy(t => t.Name)
+                    .Take(20)
+                    .AsQueryable();
+            else
+                movies = _context.Movies
+                    .Include(i => i.RecordCarrier)
+                    .Include(i => i.StorageLocation)
+                    .Include(i => i.Genre)
+                    .Where(w => w.Adult.Value == false && w.LastView == null)
+                    .OrderByDescending(o => o.CreateDate)
+                    .ThenBy(t => t.Name)
+                    .Take(20)
+                    .AsQueryable();
+
             return View(movies);
         }
 
